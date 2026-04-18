b7b4598719a84ffb83a6cede05eed8f913dab597
blokz <thebloxpl@gmail.com>
2026-04-18 09:25:04 +0200

    small fix and translation

--- UI/views/menu.py
+++ UI/views/menu.py
@@ -1,13 +1,14 @@
 import flet as ft
 from UI.components.hoverButton import HoverButton
 from UI.theme import *
-
-def menu(page: ft.Page):
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    layout = ft.Column(
-        expand=True,
-        controls=[
+class MenuView(ft.Column):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self._page = page
+        self.expand = True
+        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+        self.info_text = ft.Text("", size=16, color=PRIMARY_TEXT)
+        self.controls = [
             ft.Container(
                 content=ft.Text("日本語暗記", size=70, weight=ft.FontWeight.BOLD),
                 padding=ft.padding.only(top=70),
@@ -15,17 +16,20 @@ def menu(page: ft.Page):
             ft.Container(expand=True),
             ft.Column(
                 controls=[
-                    HoverButton("Fiszki"),
-                    HoverButton("Ustawienia"),
-                    HoverButton("Wyjście"),
+                    HoverButton("Start", on_click=self._on_fiszki_click),
+                    HoverButton("Decks"),
+                    HoverButton("Settings"),
+                    HoverButton("Exit", on_click=lambda e: page.window.close()),
                 ],
                 spacing=20,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             ),
+            self.info_text,
             ft.Container(expand=True),
-            ft.Text("v1.0", size=12, color=SURFACE),
-        ],
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-    )
+            ft.Text("v0.1", size=12, color=SURFACE),
+        ]
 
-    page.add(layout)
+    def _on_fiszki_click(self, e):
+        deck_count = 5
+        self.info_text.value = f"Masz {deck_count} decków"
+        self.info_text.update()
