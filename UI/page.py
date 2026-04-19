9bc668a9f3d676a3544bccf54c17dff160cd346b
blokz <thebloxpl@gmail.com>
2026-04-19 16:51:53 +0200

    adjusted navigation

--- UI/page.py
+++ UI/page.py
@@ -1,6 +1,4 @@
 import flet as ft
-
-from UI.components.flashcard import Flashcard
 from utils import check_level_up
 from UI.views.menu import MenuView
 
@@ -9,25 +7,24 @@ class JapaneseApp:
         self.page = page
         self.xp = 0
         self.level = 0
-        self.setup_ui()
+        self.setup_page()
+        self.navigate(MenuView)
 
-    def setup_ui(self):
+    def setup_page(self):
         self.page.title = "日本語暗記"
         self.page.theme_mode = ft.ThemeMode.DARK
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
-        self.page.add(MenuView(self.page))
-        # self.page.add(
-        #     Flashcard(self.page, "私", "わたし").view
-        # )
+
+    def navigate(self, view_class, **kwargs):
+        self.page.controls.clear()
+        self.page.add(view_class(self.navigate, **kwargs))
         self.page.update()
 
     @check_level_up
-    def add_xp(self, e):
-        self.xp += 40
-        self.stats_text.value = f"Poziom: {self.level} | XP: {self.xp}"
-        self.page.update()
+    def add_xp(self, amount: int = 40):
+        self.xp += amount
 
     def show_level_up_dialog(self):
-        self.page.snack_bar = ft.SnackBar(ft.Text("おめでとう!"))
+        self.page.snack_bar = ft.SnackBar(ft.Text("おめでとう! Level up!"))
         self.page.snack_bar.open = True
         self.page.update()
