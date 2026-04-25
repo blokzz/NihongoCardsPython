d0e566686ac594eb517061dadef7d6ae49914954
blokz <thebloxpl@gmail.com>
2026-04-25 21:19:33 +0200

    updated onhover

--- UI/components/hoverButton.py
+++ UI/components/hoverButton.py
@@ -1,7 +1,7 @@
 import flet as ft
 from UI.theme import *
 class HoverButton(ft.Container):
-    def __init__(self, label: str, on_click=None):
+    def __init__(self, label: str, on_click=None , data=None):
         super().__init__(
             content=ft.Text(label, color=PRIMARY_TEXT, size=16, weight=ft.FontWeight.W_500),
             width=BTN_WIDTH,
@@ -12,7 +12,7 @@ class HoverButton(ft.Container):
             animate=ft.Animation(duration=200, curve=ft.AnimationCurve.EASE_IN_OUT),
             on_hover=self._on_hover,
             on_click=on_click,
-        )
+            data=data        )
 
     def _on_hover(self, e):
         self.bgcolor = PRIMARY if e.data else SURFACE
