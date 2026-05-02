2082ff71afd333f1138e27d8729d34f39b34b7bb
blokz <thebloxpl@gmail.com>
2026-05-02 20:55:08 +0200

    adjusted card model

--- data/models.py
+++ data/models.py
@@ -1,10 +1,14 @@
 from datetime import date
 class Card:
-    def __init__(self, id: int, front: str, back: str, next_review: date):
+    def __init__(self, id: int, front: str, back: str, card_type: str, deck_id: int, next_review: date = None , example: str = None, reading: str = None):
         self.id = id
         self.front = front
         self.back = back
+        self.card_type = card_type
+        self.deck_id = deck_id
         self.next_review = next_review
+        self.example = example
+        self.reading = reading
 
 class Deck:
     def __init__(self, id: int, name: str):
