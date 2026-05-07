6af6279e991993fa811dc6146cb3ca59b06ca68b
blokz <thebloxpl@gmail.com>
2026-05-07 21:29:20 +0200

    updated global themes

--- UI/theme.py
+++ UI/theme.py
@@ -2,6 +2,8 @@ import flet as ft
 
 PRIMARY = ft.Colors.RED_500
 PRIMARY_TEXT = ft.Colors.WHITE
+SECONDARY_TEXT = ft.Colors.BLACK
 BG_BUTTON = ft.Colors.GREY_800
 SURFACE = ft.Colors.GREY_800
 BTN_WIDTH = 250
+BG_APP = ft.Colors.BLACK
